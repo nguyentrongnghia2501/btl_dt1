d3225ed917a6f6c7f074854ebeeac6b793eb6cfa
nghia2501 <tranphonglan212@gmail.com>
2021-11-02 21:30:25 +0700

    a

--- ntn_blog/user/index.php
+++ ntn_blog/user/index.php
@@ -7,21 +7,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        
+
     <link rel="stylesheet" href="../public/css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <!--link font  -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;600&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-<link href="https://fonts.googleapis.com/css2?family=Anton&family=Orbitron:wght@500;600&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<title>Trọn Nghĩa BLog</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;600&family=Ubuntu:wght@300&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Anton&family=Orbitron:wght@500;600&family=Ubuntu:wght@300&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <title>Trọn Nghĩa BLog</title>
 
 </head>
 
@@ -36,32 +43,65 @@
                 <nav>
                     <ul id="main_menu">
                         <li><a href="#">Trang Chủ</a></li>
-                        <li><a href="#">Thông Báo <i class="far fa-bell">  <span class="badge bg-danger">4</span></i>
                         
-                      </a></li>
+                          <!-- Thông Báo  -->
+<li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Thông Báo <i class="far fa-bell"> <span class="badge bg-danger">4</span></i>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                          
                         <li><a href="#">Xem Điểm</a></li>
                         <li><a href="#">Môn Học</a></li>
-                        <li><a href="#">LOGIN / REGISTER</a></li>
+                       
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="../public/images/g2.jpg" alt="Avatar Logo" style="width:40px;"
+                                        class="rounded-pill">
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                              
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                      
                     </ul>
-                    
+
                 </nav>
-              
+
         </header>
         <div class="container-fulid banner">
-      
-            <div id="More"style="margin-left:45%;padding-top: 12rem;">        
-            <p style="margin-left: -2rem;">Studeon Innovative & Succesfull</p>
-            <h1 style="margin-left: -17rem;text-transform: uppercase;font-family: 'Anton', sans-serif;">If you don't start working, nothing is possible. </h1>
-            <button style="color: #fff;background-color: #26b15b;width: 10rem;height: 3.5rem;" type="button" class="btn btn">More Courses</button>
+
+            <div id="More" style="margin-left:45%;padding-top: 12rem; color: azure;">
+                <p style="margin-left: -2rem;">Studeon Innovative & Succesfull</p>
+                <h1 style="margin-left: -17rem;text-transform: uppercase;font-family: 'Anton', sans-serif;">If you don't
+                    start working, nothing is possible. </h1>
+                <button style="color: #fff;background-color: #26b15b;width: 10rem;height: 3.5rem;" type="button"
+                    class="btn btn">More Courses</button>
             </div>
-            
+
         </div>
 
 
     </div>
     <!-- x -->
     <div id="conten" class="container">
-        
+
     </div>
     <!-- x -->
     <div class="container"></div>
@@ -69,26 +109,9 @@
     <div class="container-fulid">
         <div class="row">
             <div class="col-md-4" style="background-color: #fff;">x</div>
-            <div class="col-md-4" style="background-color: red;font-family:  'Ubuntu', sans-serif;" >
-        
-            Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-<br>Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-Bản dịch
-Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các định nghĩa bằng ngôn ngữ mẹ đẻ của bạn bằng cách sử dụng bộ từ điển dịch thuật thông báo Cambridge và từ điển Password và Global từ K Dictionaries.
-        
-        </div>
+            <div class="col-md-4" style="background-color: red;font-family:  'Ubuntu', sans-serif;">
+
+            </div>
             <div class="col-md-4" style="background-color: yellow;"></div>
 
         </div>
@@ -98,14 +121,13 @@ Kiểm tra sự hiểu biết của bạn về các từ tiếng Anh với các 
     </script>
     <script>
     $(document).ready(function() {
-       $(window).scroll(function(){
-               if($(this).scrollTop()){
-                  $('header').addClass('sticky');
-               }
-               else{
+        $(window).scroll(function() {
+            if ($(this).scrollTop()) {
+                $('header').addClass('sticky');
+            } else {
                 $('header').removeClass('sticky');
-               }
-       });
+            }
+        });
     });
     </script>
 </body>
